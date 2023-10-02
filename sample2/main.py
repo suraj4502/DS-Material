dabe6fa4ffa0dba331117e975c08989bfbe4f986
suraj4502 <sy820860@gmail.com>
2023-10-02 10:00:51 +0530

    02-10-23

---
+++ sample2/main.py
@@ -0,0 +1,45 @@
+import greetings
+import  my_math as m
+
+
+def main():
+    greetings.greet()
+    o = greetings.menu()
+
+    if o == '1':
+        m.add()
+    elif o == '2':
+        m.sub()
+    elif o =='3':
+        m.mul()
+    elif o =='4':
+        try:
+            m.div()
+        except Exception as e:
+            print(e.args)
+    elif o == '5':
+        m.divisibility()
+    elif o =='6':
+        m.square()
+    else:
+        greetings.bye()
+        exit()
+
+    print()
+    print()
+    print()
+    main()
+
+
+
+
+main()
+
+
+
+# from Animation import surrounding, character
+#
+#
+# surrounding.load_surrounding()
+# character.load_chararcter()
+
